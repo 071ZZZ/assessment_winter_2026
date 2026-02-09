b2b8303b2c96cc6437fe1797395f38f066430b0a
lqy <3498507573@qq.com>
2026-02-09 08:37:43 +0800

    完成assignment0第1题：温度转换功能

--- assignment_0/src/a0_01_temperature.cpp
+++ assignment_0/src/a0_01_temperature.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-
+#include<iomanip>
+#include<sstream>
 #include "rm_a0/a0_01_temperature.hpp"
+namespace rm_a0{
+  double CelsiusToFahrenheit(double celsius) {
+    return celsius * 9.0 / 5.0 + 32.0;
+}
 
+std::string FormatFahrenheit(double fahrenheit) {//格式化输出为保留2位小数的字符串
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(2) << fahrenheit;
+    return oss.str();
+}
+}
 int main() {
   double celsius = 0.0;
   if (!(std::cin >> celsius)) {
---
+++ assignment_0/src/rm_a0/a0_01_temperature.hpp
@@ -0,0 +1,8 @@
+#ifndef RM_A0_01_temperature_HPP
+#define RM_A0_01_TEMPERATURE_HPP
+#include<string>
+namespace rm_a0{
+    double CelsiusToFahrenheit(double celsius);
+    std:: string FormatFahrenheit(double fahrenheit);
+}
+#endif 
