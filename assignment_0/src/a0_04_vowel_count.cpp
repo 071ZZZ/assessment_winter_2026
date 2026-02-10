8f40783119b5f5ad4a843e32d73e46a829a9580a
lqy <3498507573@qq.com>
2026-02-10 18:21:13 +0800

    完成a0_04统计元音

--- assignment_0/src/a0_04_vowel_count.cpp
+++ assignment_0/src/a0_04_vowel_count.cpp
@@ -3,7 +3,18 @@
 #include <string>
 
 #include "rm_a0/a0_04_vowel_count.hpp"
-
+namespace rm_a0{
+  int CountVowels(std::string line){
+    int count=0;
+    for(char c:line){
+      char lower_c=std::tolower(c);
+      if(lower_c == 'a' || lower_c == 'e' || lower_c == 'i' || lower_c == 'o' || lower_c == 'u'){
+        ++count;
+      }
+    }
+    return count;
+  }
+}
 int main() {
   std::string line;
   if (!std::getline(std::cin, line)) {
---
+++ assignment_0/src/rm_a0/a0_04_vowel_count.hpp
@@ -0,0 +1,8 @@
+#ifndef a0_04_vowel_count_hpp
+#define a0_04_vowel_count_hpp
+#include<string>
+#include<cctype>
+namespace rm_a0{
+    int CountVowels(std::string line);
+}
+#endif 
