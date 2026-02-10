e55e46b28c70054e91bc517fca6f1e7c32d6e8a0
lqy <3498507573@qq.com>
2026-02-10 12:47:49 +0800

    完成a0_02闰年判断

--- assignment_0/src/a0_02_leap_year.cpp
+++ assignment_0/src/a0_02_leap_year.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 
 #include "rm_a0/a0_02_leap_year.hpp"
-
+namespace rm_a0{
+  bool IsLeapYear(int year){
+    return (year%4==0 && year%100!=0) || (year%400==0);
+  }
+  std::string FormatLeapYearAnswer(bool is_leap){
+    if(is_leap){
+      return "YES";
+    }
+    else
+      return "NO";
+  }
+}
 int main() {
   int year = 0;
   if (!(std::cin >> year)) {
---
+++ assignment_0/src/rm_a0/a0_02_leap_year.hpp
@@ -0,0 +1,9 @@
+#ifndef a0_02_leap_year_hpp
+#define a0_02_leap_year_hpp
+#include<string>
+namespace rm_a0{
+  bool IsLeapYear(int year);
+  std::string FormatLeapYearAnswer(bool is_leap);
+} 
+
+#endif
