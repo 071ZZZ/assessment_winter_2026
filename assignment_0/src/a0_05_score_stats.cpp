c3f76d495437bdd78949570182f78218951e515d
lqy <3498507573@qq.com>
2026-02-11 13:21:27 +0800

    完成a0_05简单成绩统计

--- assignment_0/src/a0_05_score_stats.cpp
+++ assignment_0/src/a0_05_score_stats.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <iomanip>
+#include <vector>
 
 #include "rm_a0/a0_05_score_stats.hpp"
+namespace rm_a0{
+  struct student {
+    std::string name;
+    int score;
+  };
+    std::string SolveScoreStats(std::string s,bool& ok){
+      ok = false;
+      std::istringstream iss(s);
+      int n;
+      iss >> n;
+      std::vector<student> scores;
+      int total = 0;
+      int max = -1;
+      std::string max_name;
+      for (int i=0;i<n;i++){
+        std::string name;
+        int score;
+        if(!(iss >> name >> score)){
+          return "";
+        }
+        scores.push_back({name,score});
+        total += score;
+        if(score>max){
+          max=score;
+          max_name=name;
+        }
+      }
+      double average = static_cast<double>(total) / n;
+      std::ostringstream oss;
+      oss << "top=" << max_name << " " << max << "\n";
+      oss << std::fixed << std::setprecision(2) << "avg:" << average;
+      ok = true;
+      return oss.str();
+    } 
+}
 
 int main() {
   std::ostringstream oss;
---
+++ assignment_0/src/rm_a0/a0_05_score_stats.hpp
@@ -0,0 +1,7 @@
+#ifndef a0_05_score_stats_hpp
+#define a0_05_score_stats_hpp
+#include <string>
+namespace rm_a0{
+    std::string SolveScoreStats(std::string s,bool& ok);
+}
+#endif 
